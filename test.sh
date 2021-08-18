1d77ece94664ee38be4e68fd9de54af56dc9f801
Craig Cheetham <craig@craigcheetham.com>
2021-08-18 21:00:29 +0100

    Got DNS working properly.

--- test.sh
+++ test.sh
@@ -6,6 +6,11 @@
 ping -c 5 8.8.8.8 > /dev/null || 
     (echo 'ERROR: Failed to connect to internet. Please check your network allows ping, and is connected.')
 
+# Test DNS
+host www.craigcheetham.com > /dev/null || 
+    (echo 'ERROR: Failed to use DNS to known working website.')
+
+
 # That local network is not allowed.
 ping -c 5 10.255.108.1 > /dev/null &&
     (echo 'ERROR: The router responded to pings. This means that LAN connectivity is not prevented.')
